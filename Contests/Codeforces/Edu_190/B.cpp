39b08c9bed0e0405509e066cba084b0774c0add5
master-hkshenoycse <hkshenoy.dev@gmail.com>
2026-05-20 07:30:49 +0530

    20260519: LC_2540 and CF_Edu_190 LC_2540: Implementation, merge sort merge step traversal

---
+++ Contests/Codeforces/Edu_190/B.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+#include <iterator>
+#include <iostream>
+#include <numeric>
+#include <math.h>
+#define ll long long
+#define ull long
+#define mpa make_pair
+#define pb push_back
+#define ff first
+#define pii pair<ll,ll>
+#define dd long double
+#define trace(x) cerr << #x << " : " << x << endl
+#define ss second
+#define boost ios_base::sync_with_stdio(0)
+#define forp(i,a,b) for(ll i=a;i<=b;i++)
+#define rep(i,n)    for(ll i=0;i<n;++i)
+#define ren(i,n)    for(ll i=n-1;i>=0;i--)
+#define forn(i,a,b) for(ll i=a;i>=b;i--)
+#define all(c) (c).begin(),(c).end()
+#define tr(c,i) for(typeof((c).begin()) i = (c).begin(); i != (c).end();
+#define sc(x) scanf("%lld",&x)
+#define clr(x,val) memset(x,val,sizeof(x))
+#define pr(x) printf("%lld\n",x) 
+#define gc getchar
+#define pdd pair<dd,dd>
+#define prec(x) cout<<fixed<<setprecision(x)
+#define fre freopen("rental.in","r",stdin),freopen("rental.out","w",stdout)
+#define arr array 
+using namespace std;
+
+void solve(ll tc){
+
+    string s;
+    cin>>s;
+
+    ll n=s.size();
+    ll cnt4=0,cnt2=0;
+    vector<ll> pref2(n+1,0);
+    for(ll i=0;i<n;i++){
+        if(s[i]=='4')cnt4++;
+        if(s[i]=='2')cnt2++;
+        pref2[i+1]=cnt2;
+    }
+
+    ll ret=cnt2;
+
+    //213/231
+    ll suff_len=0;
+    for(ll i=n;i>=1;i--){
+        if(s[i-1]=='4'){
+            continue;
+        }
+        if(s[i-1]!='2')suff_len++;
+        ret=max(ret,suff_len+pref2[i-1]); 
+    }
+
+    cout<<n-ret<<endl;
+
+}
+int main(){
+    boost;
+
+    //pre_cum();
+    //prec(10);
+	//fre;
+
+
+    ll t=1;
+    ll tc=1;
+    cin>>t;
+
+	while(t--){
+		solve(tc);
+        tc++;
+    
+	}
+
+    return 0;
+    
+     
+}
