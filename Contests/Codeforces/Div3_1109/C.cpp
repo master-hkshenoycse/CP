89b463f3a6eace24ab71fdbdcc7a45a6f940591c
master-hkshenoycse <hkshenoy.dev@gmail.com>
2026-07-15 07:40:11 +0530

    20260715:  LC_3658: return n

---
+++ Contests/Codeforces/Div3_1109/C.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+#include <iterator>
+#include <iostream>
+#include <numeric>
+#include <math.h>
+#define ll long long
+#define ull long
+#define mpa make_pair
+#define pb push_back
+#define ff first
+#define pii pair<ll,ll>
+#define dd long double
+#define trace(x) cerr << #x << " : " << x << endl
+#define ss second
+#define boost ios_base::sync_with_stdio(0)
+#define forp(i,a,b) for(ll i=a;i<=b;i++)
+#define rep(i,n)    for(ll i=0;i<n;++i)
+#define ren(i,n)    for(ll i=n-1;i>=0;i--)
+#define forn(i,a,b) for(ll i=a;i>=b;i--)
+#define all(c) (c).begin(),(c).end()
+#define tr(c,i) for(typeof((c).begin()) i = (c).begin(); i != (c).end();
+#define sc(x) scanf("%lld",&x)
+#define clr(x,val) memset(x,val,sizeof(x))
+#define pr(x) printf("%lld\n",x) 
+#define gc getchar
+#define pdd pair<dd,dd>
+#define prec(x) cout<<fixed<<setprecision(x)
+#define fre freopen("rental.in","r",stdin),freopen("rental.out","w",stdout)
+#define arr array 
+using namespace std;
+void dfs(ll v,vector<vector<ll> > &adj,vector<ll> &vis,vector<ll> &comp){
+    vis[v]=1;
+    comp.pb(v);
+
+    for(auto u:adj[v]){
+        if(vis[u]==0){
+            dfs(u,adj,vis,comp);
+        }
+    }
+}
+
+void solve(ll tc){
+    ll n,x,y;
+    cin>>n>>x>>y;
+
+    vector<vector<ll> > adj(n+1);
+    for(ll i=1;i<=n;i++){
+        if(i+x<=n){
+            adj[i].pb(i+x);
+            adj[i+x].pb(i);
+        }
+
+        if(i+y<=n){
+            adj[i].pb(i+y);
+            adj[i+y].pb(i);
+        }
+    }
+
+    vector<ll> p(n+1);
+    for(ll i=1;i<=n;i++){
+        cin>>p[i];
+    }
+
+    vector<ll> vis(n+1,0);
+    for(ll i=1;i<=n;i++){
+        if(vis[i]==0){
+            vector<ll> comp;
+            dfs(i,adj,vis,comp);
+
+
+            vector<ll> vals;
+            for(auto c:comp)
+                vals.pb(p[c]);
+            
+            sort(vals.begin(),vals.end());
+            sort(comp.begin(),comp.end());
+            for(ll i=0;i<comp.size();i++){
+                p[comp[i]]=vals[i];
+            }
+        }
+    }
+
+
+    for(ll i=2;i<=n;i++){
+        if(p[i]<p[i-1]){
+            cout<<"NO"<<endl;
+            return;
+        }
+    }
+
+    cout<<"YES"<<endl;
+
+
+
+}
+int main(){
+    boost;
+
+    //pre_cum();
+    //prec(10);
+	//fre;
+
+
+    ll t=1;
+    ll tc=1;
+    cin>>t;
+
+	while(t--){
+		solve(tc);
+        tc++;
+    
+	}
+
+    return 0;
+    
+     
+}
---
+++ DailyPractice/20260715/LC_3658.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
